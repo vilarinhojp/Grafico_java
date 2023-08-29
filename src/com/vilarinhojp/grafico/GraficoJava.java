152ea971d3689079b2436bba531e3f95fdaebc2a
Jp <joaopedrovilarinhodasilva@gmail.com>
2023-08-29 17:11:12 -0300

    Adicionando comentários

--- src/com/vilarinhojp/grafico/GraficoJava.java
+++ src/com/vilarinhojp/grafico/GraficoJava.java
@@ -4,24 +4,35 @@ import java.awt.Canvas;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-public class GraficoJava extends Canvas{
-
+//Classe GraficoJava que herda de Canvas (elemento gráfico)
+public class GraficoJava extends Canvas {
+	
+ // Variável estática que representa a janela do programa
 	public static JFrame frame;
-
+	
+	// Constantes para largura, altura e escala da janela
+	private final int WIDTH = 400;
+	private final int HEIGHT = 400;
+	private final int SCALE = 2;
+	
+	// Construtor da classe GraficoJava
 	public GraficoJava() {
-		this.setPreferredSize(new Dimension(500,500));
-		frame=new JFrame();
-		frame.add(this);
-		frame.setResizable(false);
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+		// Define o tamanho preferencial do Canvas (elemento gráfico)
+		this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		
+		// Criação da janela (frame)
+		frame = new JFrame();
+		frame.add(this); // Adiciona o Canvas à janela
+		frame.setResizable(false); // Impede redimensionamento da janela
+		frame.setVisible(true); // Torna a janela visível
+		frame.setLocationRelativeTo(null); // Centraliza a janela na tela
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Define o comportamento ao fechar a janela
+		frame.pack(); // Redimensiona a janela para se ajustar ao tamanho do Canvas
 	}
-	public static void main(String[]args) {
-
-		GraficoJava graficoJava=new GraficoJava();
-
-
+	
+	// Método main (ponto de entrada do programa)
+	public static void main(String[] args) {
+		// Criação de um objeto GraficoJava
+		GraficoJava graficoJava = new GraficoJava();
 	}
 }
