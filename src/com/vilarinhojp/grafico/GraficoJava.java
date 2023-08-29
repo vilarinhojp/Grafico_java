80c4bec5a8ec5f564d5366b3846d9ef2ff90b1df
Jp <joaopedrovilarinhodasilva@gmail.com>
2023-08-29 15:53:52 -0300

    Implementando a janela

---
+++ src/com/vilarinhojp/grafico/GraficoJava.java
@@ -0,0 +1,27 @@
+package com.vilarinhojp.grafico;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class GraficoJava extends Canvas{
+	
+	public static JFrame frame;
+	
+	public GraficoJava() {
+		this.setPreferredSize(new Dimension(500,500));
+		frame=new JFrame();
+		frame.add(this);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+	}
+	public static void main(String[]args) {
+	
+		GraficoJava graficoJava=new GraficoJava();
+		
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Grafico_java {
+	requires java.desktop;
+}
