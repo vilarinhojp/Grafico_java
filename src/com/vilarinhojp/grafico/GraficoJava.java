b1a901e4f167b93a3d0bba6356023bc6e5664566
Jp <joaopedrovilarinhodasilva@gmail.com>
2023-09-03 11:44:25 -0300

    Criando Buffer de imagem

--- src/com/vilarinhojp/grafico/GraficoJava.java
+++ src/com/vilarinhojp/grafico/GraficoJava.java
@@ -1,7 +1,11 @@
 package com.vilarinhojp.grafico;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
@@ -14,7 +18,9 @@ public class GraficoJava extends Canvas implements Runnable{
 	private final int SCALE=1;
 	private boolean isRunning=false;
 	
-	private Thread thread;
+	private Thread threadRun;
+	
+	private BufferedImage image;
 	
 	public void initFrame() {
 		this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
@@ -26,9 +32,9 @@ public class GraficoJava extends Canvas implements Runnable{
 		frame.setVisible(true);
 	}
 	public synchronized void start() {
-		thread=new Thread(this);
+		threadRun=new Thread(this);
 		isRunning=true;
-		thread.start();
+		threadRun.start();
 	}
 	public synchronized void stop() {
 		
@@ -37,7 +43,15 @@ public class GraficoJava extends Canvas implements Runnable{
 		
 	}
 	public void render() {
-		
+		BufferStrategy bs=this.getBufferStrategy();
+		if(bs==null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g =image.getGraphics();
+		g=bs.getDrawGraphics();
+		g.drawImage(image,0,0,WIDTH*SCALE,HEIGHT*SCALE,null);
+		bs.show();
 	}
 	public void run() {
 		
@@ -57,9 +71,10 @@ public class GraficoJava extends Canvas implements Runnable{
 			
 			if(delta>=1) {
 				tick();
-				render();			
-				delta--;
+				render();
 				frames++;
+				delta--;
+				
 				
 			}
 			if(System.currentTimeMillis()-timer>=1000) {
@@ -72,7 +87,7 @@ public class GraficoJava extends Canvas implements Runnable{
 	public GraficoJava() {
 		frame=new JFrame();
 		initFrame();
-		
+		image=new BufferedImage(WIDTH*SCALE,HEIGHT*SCALE,BufferedImage.TYPE_INT_RGB);
 	}
 	
 	public static void main(String[] args) {
